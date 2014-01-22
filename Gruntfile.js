e44979971e045726090b064e268508fcba4a7dda
Luke Thomas <luk3thomas@gmail.com>
2014-01-21 22:26:05 -0600

    add banner to dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-line-remover');
+    grunt.loadNpmTasks('grunt-banner');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -21,6 +22,16 @@ module.exports = function(grunt) {
         jshint: {
             main: {
                 src: [ 'Gruntfile.js', 'src/**', 'test/**' ]
+            }
+        },
+        usebanner: {
+            js: {
+                options: {
+                    banner: '// version <%= pkg.version %>'
+                },
+                files: {
+                    src: [ 'dist/eventer.<%= pkg.version %>*', 'dist/eventer.js', 'dist/eventer.min.js' ]
+                }
             }
         },
         lineremover: {
@@ -54,7 +65,7 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('default', [ 'watch' ]);
-    grunt.registerTask('build', [ 'copy', 'uglify' ]);
+    grunt.registerTask('build', [ 'copy', 'uglify', 'usebanner' ]);
     grunt.registerTask('test', [ 'shell:test' ]);
     grunt.registerTask('copy', [ 'lineremover' ]);
 
